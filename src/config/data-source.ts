a624a89b0c198d4f99487e12fa23d187aea37fda
sandeep7567 <sandeepthakur7567@gmail.com>
2024-04-14 17:21:38 +0530

    add Config setup

---
+++ src/config/data-source.ts
@@ -0,0 +1,19 @@
+import { Config } from ".";
+import "reflect-metadata";
+import { DataSource } from "typeorm";
+import { User } from "../entity/User";
+
+export const AppDataSource = new DataSource({
+    type: "postgres",
+    host: Config.DB_HOST,
+    port: Number(Config.DB_PORT),
+    username: Config.DB_USERNAME,
+    password: Config.DB_PASSWORD,
+    database: Config.DB_NAME,
+    // Don't use this in production always as false
+    synchronize: false,
+    logging: false,
+    entities: [User],
+    migrations: [],
+    subscribers: [],
+});
