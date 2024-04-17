fd53acfe45f9b115b190eab304c710107eef9cf8
sandeep7567 <sandeepthakur7567@gmail.com>
2024-04-17 10:30:49 +0530

    add register validators

--- src/validators/register-validator.ts
+++ src/validators/register-validator.ts
@@ -5,5 +5,29 @@ export default checkSchema({
         errorMessage: "Email is required",
         notEmpty: true,
         trim: true,
+        isEmail: {
+            errorMessage: "Email should be a valid email",
+        },
+    },
+    firstName: {
+        errorMessage: "First Name is required",
+        notEmpty: true,
+        trim: true,
+    },
+    lastName: {
+        errorMessage: "Last Name is required",
+        notEmpty: true,
+        trim: true,
+    },
+    password: {
+        errorMessage: "Password is required",
+        notEmpty: true,
+        trim: true,
+        isLength: {
+            options: {
+                min: 8,
+            },
+            errorMessage: "Password length should be atleast 8 characters",
+        },
     },
 });
