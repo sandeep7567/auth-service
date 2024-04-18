afc485078db67b2128a98c30abf1690bbf8e2502
sandeep7567 <sandeepthakur7567@gmail.com>
2024-04-18 13:32:42 +0530

    add test for check user is authenticated while creating tenant

--- src/routes/tenant.ts
+++ src/routes/tenant.ts
@@ -9,6 +9,7 @@ import { TenantService } from "../services/tenantService";
 import { AppDataSource } from "../config/data-source";
 import { Tenant } from "../entity/Tenant";
 import logger from "../config/logger";
+import authenticate from "../middlewares/authenticate";
 
 const router = express.Router();
 
@@ -16,8 +17,12 @@ const tenantRepository = AppDataSource.getRepository(Tenant);
 const tenantService = new TenantService(tenantRepository);
 const tenantController = new TenantController(tenantService, logger);
 
-router.post("/", (async (req: Request, res: Response, next: NextFunction) => {
-    await tenantController.create(req, res, next);
-}) as RequestHandler);
+router.post(
+    "/",
+    authenticate as RequestHandler,
+    (async (req: Request, res: Response, next: NextFunction) => {
+        await tenantController.create(req, res, next);
+    }) as RequestHandler,
+);
 
 export default router;
--- tests/tenant/create.spec.ts
+++ tests/tenant/create.spec.ts
@@ -3,18 +3,32 @@ import { AppDataSource } from "../../src/config/data-source";
 import request from "supertest";
 import app from "../../src/app";
 import { Tenant } from "../../src/entity/Tenant";
+import createJWKSMock from "mock-jwks";
+import { Roles } from "../../src/constants";
 
 describe("POST /tenants", () => {
     let connection: DataSource;
+    let jwks: ReturnType<typeof createJWKSMock>;
+    let adminToken: string;
 
     beforeAll(async () => {
+        jwks = createJWKSMock("http://localhost:5501");
         connection = await AppDataSource.initialize();
     });
 
     beforeEach(async () => {
-        // Database truncate;
         await connection.dropDatabase();
         await connection.synchronize();
+        jwks.start();
+
+        adminToken = jwks.token({
+            sub: "1",
+            role: Roles.ADMIN,
+        });
+    });
+
+    afterEach(() => {
+        jwks.stop();
     });
 
     afterAll(async () => {
@@ -27,8 +41,10 @@ describe("POST /tenants", () => {
                 name: "Tenanat 1",
                 address: "Tenant Address 1",
             };
+
             const response = await request(app)
                 .post("/tenants")
+                .set("Cookie", [`accessToken=${adminToken}`])
                 .send(tenantData);
 
             expect(response.statusCode).toBe(201);
@@ -39,7 +55,10 @@ describe("POST /tenants", () => {
                 name: "Tenanat 1",
                 address: "Tenant Address 1",
             };
-            await request(app).post("/tenants").send(tenantData);
+            await request(app)
+                .post("/tenants")
+                .set("Cookie", [`accessToken=${adminToken}`])
+                .send(tenantData);
 
             const tenantRepository = connection.getRepository(Tenant);
 
@@ -48,6 +67,24 @@ describe("POST /tenants", () => {
             expect(tenants).toHaveLength(1);
             expect(tenants[0].name).toBe(tenantData.name);
             expect(tenants[0].address).toBe(tenantData.address);
+        });
+
+        it("should return 401 if user is not authenticated", async () => {
+            const tenantData = {
+                name: "Tenanat 1",
+                address: "Tenant Address 1",
+            };
+            const response = await request(app)
+                .post("/tenants")
+                .send(tenantData);
+
+            expect(response.statusCode).toBe(401);
+
+            const tenantRepository = connection.getRepository(Tenant);
+
+            const tenants = await tenantRepository.find();
+
+            expect(tenants).toHaveLength(0);
         });
     });
 });
