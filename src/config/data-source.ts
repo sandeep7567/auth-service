5a985970dbd36dd992d3b20acddbbb101e560074
sandeep7567 <sandeepthakur7567@gmail.com>
2024-04-18 21:56:46 +0530

    fix ts build

--- src/config/data-source.ts
+++ src/config/data-source.ts
@@ -12,7 +12,7 @@ export const AppDataSource = new DataSource({
     // Don't use this in production always as false
     synchronize: false,
     logging: false,
-    entities: ["src/entity/*.ts"],
-    migrations: ["src/migration/*.ts"],
+    entities: ["src/entity/*.{ts,js}"],
+    migrations: ["src/migration/*.{ts,js}"],
     subscribers: [],
 });
